b3fdde276295f6382765b407da307e7254748a6d
imediasun <imediasun@gmail.com>
2020-06-24 20:36:40 +0300

    bug with icon nullable resolved

--- app/Containers/Site/Data/Migrations/2018_03_01_142917_create_product_categories_table.php
+++ app/Containers/Site/Data/Migrations/2018_03_01_142917_create_product_categories_table.php
@@ -18,9 +18,9 @@ class CreateProductCategoriesTable extends Migration
             $table->bigIncrements('id');
             $table->integer('parent_id');
             $table->string('name');
-            $table->string('icon');
-            $table->string('link');
-            $table->string('photo');
+            $table->string('icon')->nullable();
+            $table->string('link')->nullable();
+            $table->string('photo')->nullable();
             $table->integer('position');
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
