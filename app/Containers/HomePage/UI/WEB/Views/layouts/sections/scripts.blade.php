dea716e46d70bc2404dfe071a10fe1534cb832b2
imediasun <imediasun@gmail.com>
2020-07-02 23:04:51 -0700

    some changes in google autocomplete

--- app/Containers/HomePage/UI/WEB/Views/layouts/sections/scripts.blade.php
+++ app/Containers/HomePage/UI/WEB/Views/layouts/sections/scripts.blade.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+
 <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
