9a3c9edd39f647a46d2fd2de93214338f47a5332
imediasun <imediasun@gmail.com>
2020-07-13 00:48:49 +0300

    some changes in help text

--- app/Containers/StaticPage/UI/WEB/Views/help/index.blade.php
+++ app/Containers/StaticPage/UI/WEB/Views/help/index.blade.php
@@ -18,17 +18,25 @@
     <main>
         <div class="container">
 <div class="row">
-    <div class="col-md-3">
-        <ul style="margin-top:50px;color:#A269F7">
-            @foreach($data['restHelpPage'] as $val)
-                <a style="color:#A269F7" href="/help/{{$val->id}}"><li style="cursor:pointer">> {{$val->name}}</li></a><br>
-            @endforeach
+    <div class="col-md-4">
+        <ul style="margin-top:0px;color:#A269F7">
+
+                <a style="color:#A269F7" href="/help/1#main"><li style="cursor:pointer">> Общие положения</li></a><br>
+            <a style="color:#A269F7" href="/help/1#adds"><li style="cursor:pointer">> Размещение объявлений</li></a><br>
+            <a style="color:#A269F7" href="/help/1#from_users"><li style="cursor:pointer">> Сведения, предоставляемые пользователем</li></a><br>
+            <a style="color:#A269F7" href="/help/1#delete_reasons"><li style="cursor:pointer">> Причины удаления объявлений</li></a><br>
+            <a style="color:#A269F7" href="/help/1#rights"><li style="cursor:pointer">> Права и обязанности сторон</li></a><br>
+            <a style="color:#A269F7" href="/help/1#pays"><li style="cursor:pointer">> Оплата услуг</li></a><br>
+            <a style="color:#A269F7" href="/help/1#ltd"><li style="cursor:pointer">> Ограничение ответственности компании</li></a><br>
+            <a style="color:#A269F7" href="/help/1#changes"><li style="cursor:pointer">> Внесение изменений в соглашение</li></a><br>
+            <a style="color:#A269F7" href="/help/1#rest"><li style="cursor:pointer">> Прочие условия</li></a><br>
+
         </ul>
 
 
     </div>
 
-    <div class="col-md-3">
+    <div class="col-md-8">
         <?
 print($data['firstHelpPage']->content);?>
 
