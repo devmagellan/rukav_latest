9bf7b7f716321a9a6fef32716341a206e96e1077
devmagellan <dev.magellan@gmail.com>
2020-09-12 13:17:24 +0300

    fixes

---
+++ resources/views/vendor/mail/html/code.blade.php
@@ -0,0 +1,14 @@
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+<div style="font-size:20px"><span style="display:inline-block;">Ваш код: &nbsp;</span><span style="font-weight:bold;display:inline-block;font-size:22px">{{ Illuminate\Mail\Markdown::parse(substr($slot, -4)) }}</span></div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+
---
+++ resources/views/vendor/mail/html/thanks_for_registartion.blade.php
@@ -0,0 +1,14 @@
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+<p>Спасибо за регистрацию на сайте «RUKAV», Британской платформы объявлений для русскоговорящих в UK. Пожалуйста, <span style="font-weight:bold">подтвердите</span> ваш адрес электронной почты, чтобы мы могли отправлять вам уведомления о ваших объявлениях и другие важные новости.!</p>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+
