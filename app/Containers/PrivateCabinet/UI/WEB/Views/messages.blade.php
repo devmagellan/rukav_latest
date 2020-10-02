e4101262b4ae598764fa31a55e083c2e221dc601
devmagellan <dev.magellan@gmail.com>
2020-10-02 23:44:42 +0300

    fixing messages

--- app/Containers/PrivateCabinet/UI/WEB/Views/messages.blade.php
+++ app/Containers/PrivateCabinet/UI/WEB/Views/messages.blade.php
@@ -256,7 +256,7 @@
                             $opponent=\App\Containers\User\Models\User::where('id',$conversation->receiver_id)->first();
                           }
                           ?>
-                        <div class="message_sidebar_theme_item message_sidebar_theme_item-new" id="conv_id_{{$conversation->id}}" onclick="reloadMessageList('{{$conversation->id}}')" >
+                        <div class="message_sidebar_theme_item @if($conversation->viewed_at==null) message_sidebar_theme_item-new @endif" id="conv_id_{{$conversation->id}}" onclick="reloadMessageList('{{$conversation->id}}')" >
                             <div class="message_sidebar_theme_left">
                                 <div class="massage_user_avatar">
                                     <?
