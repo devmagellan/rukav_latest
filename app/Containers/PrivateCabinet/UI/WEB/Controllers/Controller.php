f81366600818e9fa4cf7dc20cae965d4b4b1e50f
devmagellan <dev.magellan@gmail.com>
2020-12-03 18:13:07 +0200

    userChangePassword function

--- app/Containers/PrivateCabinet/UI/WEB/Controllers/Controller.php
+++ app/Containers/PrivateCabinet/UI/WEB/Controllers/Controller.php
@@ -231,7 +231,7 @@ class Controller extends WebController
 
   public function profileSaveToOrganisation(ProfileSaveToOrganisationRequest $request)
   {
-    \Log::info('user_after_save_to_organisation');
+    \Log::info('user_after_save_to_organisation'.$request->input('vid_user'));
     if ($request->input('vid_user') == 'Частная') {
       \Log::info('fromPrivate');
       $controller = new \App\Containers\User\UI\WEB\Controllers\Controller(new UserService());
