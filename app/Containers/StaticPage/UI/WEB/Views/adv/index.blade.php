ee28d8e6fc245bb095d2d31e8aeab186188baa06
devmagellan <dev.magellan@gmail.com>
2020-12-05 20:35:33 +0200

    adv page validation numeric in phone field

--- app/Containers/StaticPage/UI/WEB/Views/adv/index.blade.php
+++ app/Containers/StaticPage/UI/WEB/Views/adv/index.blade.php
@@ -73,7 +73,7 @@
                     <span id="lastName" class="errorBlock"></span>
                   </div>
                   <div class="col-md-11" style="margin-top:15px">
-                    <input type="tel" name="sender_phone" id="telphone5"><span class="required">*</span>
+                    <input type="tel" name="sender_phone" id="telphone5" onkeypress='validate(event)'><span class="required">*</span>
                     <span id="phone" class="errorBlock"></span>
                   </div>
                 <div class="col-md-11">
@@ -114,6 +114,24 @@
 @section('scripts')
   <script src='https://www.google.com/recaptcha/api.js'></script>
   <script>
+    function validate(evt) {
+      var theEvent = evt || window.event;
+
+      // Handle paste
+      if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+      } else {
+        // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+      }
+      var regex = /[0-9]|\./;
+      if( !regex.test(key) ) {
+        theEvent.returnValue = false;
+        if(theEvent.preventDefault) theEvent.preventDefault();
+      }
+    }
+
 
     $('.contact_submit').on('click', function(e) {
       e.preventDefault();
